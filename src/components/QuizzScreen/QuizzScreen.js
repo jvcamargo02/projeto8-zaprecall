0443e187750dfa4c13fd607598d87bf835d2fa4c
jvcamargo02 <joaovcamargo2002@gmail.com>
2022-05-14 11:29:44 -0300

    Implement shuffle flahscards on QuizzScreen component

--- src/components/QuizzScreen/QuizzScreen.js
+++ src/components/QuizzScreen/QuizzScreen.js
@@ -1,7 +1,48 @@
 export default function QuizzScreen () {
+
+    const flashCards = [{
+        question: 'O que é JSX?',
+        answer: 'Uma extensão de linguagem do JavaScript'
+        },{
+        question: 'O React é __',
+        answer: 'uma biblioteca JavaScript para construção de interfaces'
+        },{
+        question: 'Componentes devem iniciar com __',
+        answer: 'letra maiúscula'
+        },{
+        question: 'Podemos colocar __ dentro do JSX',
+        answer: 'expressões'
+        },{
+        question: 'O ReactDOM nos ajuda __ ',
+        answer: 'interagindo com a DOM para colocar componentes React na mesma'
+        },{
+        question: 'Usamos o npm para __',
+        answer: 'gerenciar os pacotes necessários e suas dependências'
+        },{
+        question: 'Usamos props para __ ',
+        answer: 'passar diferentes informações para componentes '
+        },{
+        question: 'Usamos estado (state) para __',
+        answer: 'dizer para o React quais informações quando atualizadas devem renderizar a tela novamente'
+        }]
+
+        function shuffle () {
+            flashCards.sort(comparador)
+            
+        }
+
+        function comparador() {
+            return Math.random() - 0.5;
+        }
+
+        shuffle()
     return(
-        <>
-            <h1>tchau</h1>
-        </>
+        <div>
+            {
+                flashCards.map((flashcard,index) => (<p>{flashcard.question}</p>))
+        }
+        </div>
+             
+        
     )
 }
