25d5a0592a97e5003b9582f8d7857926dfe94fb7
jvcamargo02 <joaovcamargo2002@gmail.com>
2022-05-15 07:24:17 -0300

    footer icons

--- src/components/QuizzScreen/QuizzScreen.js
+++ src/components/QuizzScreen/QuizzScreen.js
@@ -2,132 +2,146 @@ import React from 'react';
 import './style.css'
 import revertImg from '../../assets/images/Vector.png'
 
-export default function QuizzScreen () {
+export default function QuizzScreen() {
 
-    const flashCards =  [{
+    const flashCards = [{
         question: 'O que é JSX?',
         answer: 'Uma extensão de linguagem do JavaScript',
         isVisible: false
-        },{
+    }, {
         question: 'O React é __',
         answer: 'uma biblioteca JavaScript para construção de interfaces',
         isVisible: false
-        },{
+    }, {
         question: 'Componentes devem iniciar com __',
         answer: 'letra maiúscula',
         isVisible: false
-        },{
+    }, {
         question: 'Podemos colocar __ dentro do JSX',
         answer: 'expressões',
         isVisible: false
-        },{
+    }, {
         question: 'O ReactDOM nos ajuda __ ',
         answer: 'interagindo com a DOM para colocar componentes React na mesma',
         isVisible: false
-        },{
+    }, {
         question: 'Usamos o npm para __',
         answer: 'gerenciar os pacotes necessários e suas dependências',
         isVisible: false
-        },{
+    }, {
         question: 'Usamos props para __ ',
         answer: 'passar diferentes informações para componentes ',
         isVisible: false
-        },{
+    }, {
         question: 'Usamos estado (state) para __',
         answer: 'dizer para o React quais informações quando atualizadas devem renderizar a tela novamente',
         isVisible: false
-        }]
+    }]
 
-        function shuffle () {
-            flashCards.sort(comparador)
-            
-        }
+    function shuffle() {
+        flashCards.sort(comparador)
 
-        function comparador() {
-            return Math.random() - 0.5;
-        }
+    }
 
-        const [answered, setAnswered] = React.useState(0)
+    function comparador() {
+        return Math.random() - 0.5;
+    }
 
-        shuffle()
- 
-    return(
+    const [answered, setAnswered] = React.useState(0)
+    const [footerIcons, setFooterIcons] = React.useState([])
+    shuffle()
+
+    return (
         <div className="quizz-screen">
             <Header />
-            {flashCards.map((flashCard,index) => <RenderFlashCard key={index} index={index} visible={flashCard.isVisible} question={flashCard.question} answer={flashCard.answer} footerNum={answered} refreshFooter={setAnswered}/>)}
-            <Footer numQuestions={flashCards.length} answered={answered}/>
+            {flashCards.map((flashCard, index) => <RenderFlashCard key={index} index={index} visible={flashCard.isVisible} question={flashCard.question} answer={flashCard.answer} footerNum={answered} refreshFooter={setAnswered} footerIcons={footerIcons} setFooterIcons={setFooterIcons} />)}
+            <Footer numQuestions={flashCards.length} answered={answered} footerIcons={footerIcons}/>
         </div>
     )
 }
 
 
-function Header (){
-    return(
+function Header() {
+    return (
         <header>
-            <img src="https://imagensemoldes.com.br/wp-content/uploads/2020/04/Imagem-Raio-PNG.png" alt="Logo"/>
+            <img src="https://imagensemoldes.com.br/wp-content/uploads/2020/04/Imagem-Raio-PNG.png" alt="Logo" />
             <h1 className="logo-name">ZapRecall</h1>
         </header>
-        
+
     )
 }
 
-function Footer ({numQuestions, answered}){
-    return(
-        <footer>
-            {answered}/{numQuestions} CONCLUÍDOS
-        </footer>
+function Footer({ numQuestions, answered, footerIcons }) {
+    console.log(footerIcons)
+    return (
+        footerIcons.length === 0 ?  
+            <footer>
+                {answered}/{numQuestions} CONCLUÍDOS
+            </footer> : 
+            <footer>
+                {answered}/{numQuestions} CONCLUÍDOS
+                <div className='icons'>
+                {footerIcons.map((footerIcon) => <ion-icon name={footerIcon}></ion-icon>)}
+                </div>
+            </footer>
     )
 }
 
 
-function RenderFlashCard ({index, visible, question, answer, footerNum, refreshFooter}) {
+function RenderFlashCard({ index, visible, question, answer, footerNum, refreshFooter, footerIcons, setFooterIcons }) {
 
     const [isVisible, setIsVisible] = React.useState(visible)
     const [status, setStatus] = React.useState('flashcard')
     const [ionicon, setIonicon] = React.useState('play-outline')
 
-    function answered (props) {
+    function answered(props) {
         setIsVisible(false)
         refreshFooter(footerNum + 1)
 
-        if(props === 'zap') {
+        if (props === 'zap') {
             setStatus('flashcard zap')
             setIonicon('checkmark-circle')
+            const footer = [...footerIcons, 'checkmark-circle']
+            setFooterIcons(footer)
         } else if (props === 'middle') {
             setStatus('flashcard middle')
             setIonicon('help-circle')
+            const footer = [...footerIcons, 'help-circle']
+            setFooterIcons(footer)
         } else {
             setStatus('flashcard wrong')
             setIonicon('close-circle')
+            const footer = [...footerIcons, 'close-circle']
+            setFooterIcons(footer)
         }
-        
+
     }
 
 
-    if(isVisible === false && status === 'flashcard') {
-        return(
-                <div onClick={() => setIsVisible("question")} className={status}>
-                    <p>Pergunta {index + 1}</p>
-                    <ion-icon name={ionicon}></ion-icon>
-                </div> 
+    if (isVisible === false && status === 'flashcard') {
+        return (
+            <div onClick={() => setIsVisible("question")} className={status}>
+                <p>Pergunta {index + 1}</p>
+                <ion-icon name={ionicon}></ion-icon>
+            </div>
         )
-    } else if(isVisible === false) {
-        return(
+    } else if (isVisible === false) {
+        return (
             <div className={status}>
                 <p>Pergunta {index + 1}</p>
                 <ion-icon name={ionicon}></ion-icon>
-            </div> 
+            </div>
         )
     } else if (isVisible === "question") {
-        return(
+        return (
             <div className='flashcard open'>
                 <p >{question}</p>
-                <img onClick={() => setIsVisible("answer")} src={revertImg} />
+                <img onClick={() => setIsVisible("answer")} src={revertImg} alt="Revert Card"/>
             </div>
-            
+
         )
     } else if (isVisible === "answer") {
-        return(
+        return (
             <div className='flashcard open answer'>
                 <p>{answer}</p>
                 <div className='zap-btns'>
